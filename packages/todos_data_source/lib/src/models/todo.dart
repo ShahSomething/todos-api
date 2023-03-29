107f73988534a030b16b0f7a46c3144a47ee62e8
Shah Raza <shahsomething@yahoo.com>
2023-03-29 05:26:49 +0500

    Initial commit

---
+++ packages/todos_data_source/lib/src/models/todo.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
+
+@immutable
+class Todo {
+  Todo({
+    this.id,
+    required this.title,
+    this.description = '',
+    this.isCompleted = false,
+  }) : assert(id == null || id.isNotEmpty, 'id cannot be empty');
+
+  final String? id;
+
+  final String title;
+
+  final String description;
+
+  final bool isCompleted;
+
+  List<Object?> get props => [id, title, description, isCompleted];
+
+  Todo copyWith({
+    String? id,
+    String? title,
+    String? description,
+    bool? isCompleted,
+  }) {
+    return Todo(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'description': description,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  factory Todo.fromMap(Map<String, dynamic> map) {
+    return Todo(
+      id: map['id'] != null ? map['id'] as String : null,
+      title: map['title'] as String,
+      description: map['description'] as String,
+      isCompleted: map['isCompleted'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Todo.fromJson(String source) =>
+      Todo.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  bool operator ==(covariant Todo other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.title == title &&
+        other.description == description &&
+        other.isCompleted == isCompleted;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        title.hashCode ^
+        description.hashCode ^
+        isCompleted.hashCode;
+  }
+}
---
+++ routes/index.dart
@@ -0,0 +1,5 @@
+import 'package:dart_frog/dart_frog.dart';
+
+Response onRequest(RequestContext context) {
+  return Response(body: 'TODOs backend made with dart_frog');
+}
